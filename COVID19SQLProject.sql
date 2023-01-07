f3bb70e06cc19f17783a4eec29dbb23d8cffaee0
Jetano <108341933+jetano@users.noreply.github.com>
2023-01-07 15:39:14 +0800

    Update COVID19SQLProject.sql

--- COVID19SQLProject.sql
+++ COVID19SQLProject.sql
@@ -3,6 +3,8 @@
 Covid 19 Data Exploration using PostgreSQL
 Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 
+Dataset source: https://ourworldindata.org/covid-deaths
+
 */
 
 SELECT *
